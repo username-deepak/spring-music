7c5ab615b699e2bd0a6bac3cdbfcbaee2db237aa
Daxter Mateo <dmateo@pivotal.io>
2017-03-17 14:58:11 -0400

    err

--- ci/tasks/app-manifest-prep.sh
+++ ci/tasks/app-manifest-prep.sh
@@ -4,4 +4,4 @@ set -xe
 Version=`cat version/number`
 
 
-echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 512M \n  host: VZW-Spring-Music-CICD-Demo\n  buildpack: java_buildpack" > app-manifest-output/manifest.yml
+echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 512M \n  host: VZW-Spring-Music-CICD-Demo \n  path: build/spring-music-$Version.war" > app-manifest-output/manifest.yml
