969be7d20f2b124a27b6a25f1bae7da5782ae753
Daxter Mateo <dmateo@pivotal.io>
2017-03-17 15:25:22 -0400

    s

--- ci/tasks/app-manifest-prep.sh
+++ ci/tasks/app-manifest-prep.sh
@@ -4,4 +4,4 @@ set -xe
 Version=`cat version/number`
 
 
-echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 512M \n  host: VZW-Spring-Music-CICD-Demo \n  path: music-release/spring-music-$Version.war" > app-manifest-output/manifest.yml
+echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 512M \n  host: VZW-Spring-Music-CICD-Demo \n  path: /tmp/build/put/music-release/spring-music-$Version.war" > app-manifest-output/manifest.yml
