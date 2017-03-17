3572934c58d3c8954f6cea1f662e9447c6cf350b
Daxter Mateo <dmateo@pivotal.io>
2017-03-17 13:43:15 -0400

    adding deploy task and changing app manifiest output location

--- ci/tasks/app-manifest-prep.sh
+++ ci/tasks/app-manifest-prep.sh
@@ -4,4 +4,4 @@ set -xe
 Version=`cat version/number`
 
 
-echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 512M \n  host: VZW-Spring-Music-CICD-Demo \n  path: build/spring-music-$Version.war" > manifest.yml
+echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 512M \n  host: VZW-Spring-Music-CICD-Demo \n  path: build/spring-music-$Version.war" > app-manifest-output/manifest.yml
