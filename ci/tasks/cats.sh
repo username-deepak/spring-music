0e3bc8c44c42cad9fda95ef40f754830996b0ed1
DaxterM <DaxterM@users.noreply.github.com>
2017-10-06 15:05:30 -0400

    Update cats.sh

--- ci/tasks/cats.sh
+++ ci/tasks/cats.sh
@@ -3,8 +3,10 @@ set -e
 
 get_code="curl -I $MUSIC_URL 2>/dev/null | head -n 1 | cut -d$' ' -f2"
 status_code=`eval $get_code`
+echo "Making Curl request to $MUSIC_URL"
 if [ "$status_code" != "200" ]
 then
   echo "Expect status code from $MUSIC_URL as 200, but got $status_code"
   exit 1
 fi
+echo "$MUSIC_URL Reteruned a 200 response"
