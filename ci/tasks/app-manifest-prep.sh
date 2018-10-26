03ad17daf94df9a80a2999cf8d004eb93746f849
Shawn Sherwood <ssherwood@users.noreply.github.com>
2018-10-26 16:07:50 -0400

    Update app-manifest-prep.sh

--- ci/tasks/app-manifest-prep.sh
+++ ci/tasks/app-manifest-prep.sh
@@ -4,4 +4,4 @@ set -xe
 Version=`cat version/number`
 
 
-echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 1G \n  host: VZW-Spring-Music-CICD-Demo \n  path: /tmp/build/put/music-release/spring-music-$Version.war" > app-manifest-output/manifest.yml
+echo -e "applications: \n- name: Spring-Music-CIDemo \n  memory: 1G \n  random-route: true \n  path: /tmp/build/put/music-release/spring-music-$Version.war" > app-manifest-output/manifest.yml
